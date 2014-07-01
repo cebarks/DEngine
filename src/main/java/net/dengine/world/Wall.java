ad09af3eae86cc9f1eaf05d5cf9d3e47c90f4472
Maakat <matisse.am@gmail.com>
2014-07-01 15:00:33 -0600

    Started the bsp

--- src/main/java/net/dengine/world/Wall.java
+++ src/main/java/net/dengine/world/Wall.java
@@ -1,7 +1,22 @@
 package net.dengine.world;
 
+import net.dengine.vec.Line;
+import net.dengine.vec.Vector;
+
 public class Wall {
 	
+	public Line line;
 	
+	public Wall(Vector v1, Vector v2) {
+		
+		line = new Line(v1, v2);
+		
+	}
+	
+	public Wall(Line line) {
+		
+		this.line = line;
+		
+	}
 
 }
