d62ec0f2cd88af9e4957654413c1ef304df6d20b
Maakat <matisse.am@gmail.com>
2014-07-01 11:55:45 -0600

    Started the main DEngine class, and added commenting.

--- src/main/java/net/dengine/App.java
+++ src/main/java/net/dengine/App.java
@@ -4,10 +4,20 @@ package net.dengine;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+    public static void main( String[] args ) {
+    	
+        DEngine.create();
+        
+        while(DEngine.isRunning()) {
+        	
+        	DEngine.run();
+        	
+        }
+        
+        DEngine.close(0);
+        
     }
+    
 }
