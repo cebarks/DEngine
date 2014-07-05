44d69e4ca29c7625beda4b1defed2c2da2832dd2
Anten Skrabec <cebarks@gmail.com>
2014-07-04 20:21:59 -0600

    Vector3 class with basic math functions

---
+++ src/main/java/net/dengine/vec/Vector3.java
@@ -0,0 +1,71 @@
+package net.dengine.vec;
+
+public class Vector3 {
+
+	/**
+	 * The x component of the vector.
+	 */
+	public float x;
+	/**
+	 * The y component of the vector.
+	 */
+	public float y;
+	/**
+	 * The z component of the vector.
+	 */
+	public float z;
+
+	/**
+	 * Constructor for a 3D vector.
+	 *
+	 * @param x
+	 *            the x coordinate.
+	 * @param y
+	 *            the y coordinate.
+	 * @param z_
+	 *            the y coordinate.
+	 */
+	public Vector3(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/**
+	 * Constructor for an empty vector: x, y, and z are set to 0.
+	 */
+
+	public Vector3() {
+		x = 0f;
+		y = 0f;
+		z = 0f;
+	}
+
+	public Vector3 add(Vector3 v) {
+		return new Vector3(x + v.x, y + v.y, z + v.z);
+	}
+
+	public Vector3 sub(Vector3 v) {
+		return new Vector3(x - v.x, y - v.y, z - v.z);
+	}
+
+	public Vector3 mult(Vector3 v) {
+		return new Vector3(x * v.x, y * v.y, z * v.z);
+	}
+
+	public Vector3 div(Vector3 v) {
+		return new Vector3(x / v.x, y / v.y, z / v.z);
+	}
+
+	public void normalize() {
+		float m = magnitude();
+
+		x /= m;
+		y /= m;
+		z /= m;
+	}
+
+	public float magnitude() {
+		return (float) Math.sqrt((x * x) + (y * y) + (z * z));
+	}
+}
